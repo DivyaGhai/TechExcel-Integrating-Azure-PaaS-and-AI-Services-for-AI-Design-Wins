96327414513b663c657a9bb841862ded22983db7
DivyaGhai <divyaghai@ymail.com>
2025-02-21 15:07:12 +0000

    Changes

--- src/ContosoSuitesWebAPI/obj/Debug/net8.0/ContosoSuitesWebAPI.AssemblyInfo.cs
+++ src/ContosoSuitesWebAPI/obj/Debug/net8.0/ContosoSuitesWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoSuitesWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218c96ba450effda054df14a35d51e1cd7a1e84e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702d13afa21c29b52316612ab01a8076ae1da741")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoSuitesWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoSuitesWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
