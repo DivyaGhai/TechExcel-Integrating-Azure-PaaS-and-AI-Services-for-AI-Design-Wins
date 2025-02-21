7b6db488116bcd922797fcf64d1413312406e163
DivyaGhai <divyaghai@ymail.com>
2025-02-21 15:02:48 +0000

    changes

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER [id-dfz3eae5z7ulu] FROM EXTERNAL PROVIDER;
 
+ALTER ROLE db_datareader ADD MEMBER [id-dfz3eae5z7ulu];
+ALTER ROLE db_datawriter ADD MEMBER [id-dfz3eae5z7ulu];
